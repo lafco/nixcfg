ffe2a9d1e22ce947969f57cc4e702ee0b9b83ccd
lafco <vinigfalconi@outlook.com.br>
2025-07-10 17:14:18 -0300

    Bug: trying to fix modules-shrunk/lib not found

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -3,6 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd.tpm2.enable = false;
   environment.systemPackages = with pkgs; [
     sbctl
   ];
